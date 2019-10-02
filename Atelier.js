050c736eeabd5cb5aef09ce37f43abbbf3207402
Maxence Vanmarcke <MaxenceVanmarcke21@gmail.com>
2019-10-02 11:34:58 +0200

    issou

---
+++ Atelier.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk')
+
+console.log(chalk.blue('Alexis Barbier'))
+console.log(chalk.red('Baptiste Nicolas'))
+console.log(chalk.cyan('Melanie Liraud-Coel'))
+console.log(chalk.yellow('Violette Cruau'))
